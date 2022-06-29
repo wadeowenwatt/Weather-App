a5638602e5bdc71ceca861946e0ecd86c2693a84
Trần Nhật Linh <trannhatlinh120801@gmail.com>
2022-06-29 17:02:07 +0700

    change layout when click button

--- app/src/main/java/com/example/weatherapp/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,48 +31,108 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // setting button
-        binding.settingButton.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_settingFragment)
+        binding.button7Days.setOnClickListener {
+            binding.bigCard.visibility = View.GONE
+            binding.smallCard.visibility = View.VISIBLE
+            binding.button7Days.visibility = View.GONE
+            bindingData()
         }
-        binding.addButton.setOnClickListener {
-            findNavController().navigate(R.id.action_homeFragment_to_manageLocationFragment)
-        }
 
-        // set data with viewModel
-        viewModel.weatherData.observe(viewLifecycleOwner) {
-            binding.location.text = it.timezone
+        bindingData()
 
-            binding.weatherStatus.text = it.current.weather[0].main
+    }
 
-            binding.temp.text = "${(it.current.temp - 273.15).toInt()}"
+    private fun bindingData() {
+        if (binding.bigCard.visibility == View.VISIBLE) {
+            // setting button
+            binding.settingButton.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_settingFragment)
+            }
+            binding.addButton.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_manageLocationFragment)
+            }
+            // set data with viewModel
+            viewModel.weatherData.observe(viewLifecycleOwner) {
+                binding.location.text = it.timezone
 
-            val iconUrl = "http://openweathermap.org/img/wn/" + it.current.weather[0].icon + "@2x.png"
-            val uri = iconUrl.toUri().buildUpon().scheme("https").build()
-            binding.iconWeather.load(uri) {
-                placeholder(R.drawable.placeholdericon)
+                binding.weatherStatus.text = it.current.weather[0].main
+
+                binding.temp.text = "${(it.current.temp - 273.15).toInt()}"
+
+                val iconUrl =
+                    "http://openweathermap.org/img/wn/" + it.current.weather[0].icon + "@2x.png"
+                val uri = iconUrl.toUri().buildUpon().scheme("https").build()
+                binding.iconWeather.load(uri) {
+                    placeholder(R.drawable.placeholdericon)
+                }
+
+                // 4 attribute: wind, rain, pressure and humidity
+                binding.wind.text = "${it.current.wind_speed} km/h"
+                binding.chanceOfRain.text =
+                    if (it.current.rain != null) it.current.rain.`1h`.toString() + " mm"
+                    else "0 mm"
+                binding.pressure.text = "${it.current.pressure} mbar"
+                binding.humidity.text = "${it.current.humidity} %"
+
+                binding.dayInWeek.text = "${viewModel.convertEpochToDayOfWeek(it.current.dt)}"
+                binding.dayInMonth.text = "${viewModel.convertEpochToDay(it.current.dt)}"
+
+                binding.dayInWeekBold.text = "${viewModel.convertEpochToDayOfWeek(it.current.dt)}"
+                binding.dayInMonthBold.text = "${viewModel.convertEpochToDay(it.current.dt)}"
+
+                val adapter = PredictHourlyAdapter(it.hourly)
+                binding.predictHourlyRecycler.adapter = adapter
             }
 
-            // 4 attribute: wind, rain, pressure and humidity
-            binding.wind.text = "${it.current.wind_speed} km/h"
-            binding.chanceOfRain.text =
-                if (it.current.rain != null) it.current.rain.`1h`.toString() + " mm"
-                    else "0 mm"
-            binding.pressure.text = "${it.current.pressure} mbar"
-            binding.humidity.text = "${it.current.humidity} %"
-
-            binding.dayInWeek.text = "${viewModel.convertEpochToDayOfWeek(it.current.dt)}"
-            binding.dayInMonth.text = "${viewModel.convertEpochToDay(it.current.dt)}"
-
-            binding.dayInWeekBold.text = "${viewModel.convertEpochToDayOfWeek(it.current.dt)}"
-            binding.dayInMonthBold.text = "${viewModel.convertEpochToDay(it.current.dt)}"
-
-            val adapter = PredictHourlyAdapter(it.hourly)
-            binding.predictHourlyRecycler.adapter = adapter
+            viewModel.status.observe(viewLifecycleOwner) {
+                binding.weatherStatus.text = it
+            }
         }
 
-        viewModel.status.observe(viewLifecycleOwner) {
-            binding.weatherStatus.text = it
+        if (binding.smallCard.visibility == View.VISIBLE){
+            // setting button
+            binding.settingButton1.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_settingFragment)
+            }
+            binding.addButton1.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_manageLocationFragment)
+            }
+            // set data with viewModel
+            viewModel.weatherData.observe(viewLifecycleOwner) {
+                binding.location1.text = it.timezone
+
+                binding.weatherStatus1.text = it.current.weather[0].main
+
+                binding.temp1.text = "${(it.current.temp - 273.15).toInt()}"
+
+                val iconUrl =
+                    "http://openweathermap.org/img/wn/" + it.current.weather[0].icon + "@2x.png"
+                val uri = iconUrl.toUri().buildUpon().scheme("https").build()
+                binding.iconWeather1.load(uri) {
+                    placeholder(R.drawable.placeholdericon)
+                }
+
+                // 4 attribute: wind, rain, pressure and humidity
+                binding.wind1.text = "${it.current.wind_speed} km/h"
+                binding.chanceOfRain1.text =
+                    if (it.current.rain != null) it.current.rain.`1h`.toString() + " mm"
+                    else "0 mm"
+                binding.pressure1.text = "${it.current.pressure} mbar"
+                binding.humidity1.text = "${it.current.humidity} %"
+
+                binding.dayInWeek1.text = "${viewModel.convertEpochToDayOfWeek(it.current.dt)}"
+                binding.dayInMonth1.text = "${viewModel.convertEpochToDay(it.current.dt)}"
+
+                binding.dayInWeekBold.text = "${viewModel.convertEpochToDayOfWeek(it.current.dt)}"
+                binding.dayInMonthBold.text = "${viewModel.convertEpochToDay(it.current.dt)}"
+
+                val adapter = PredictHourlyAdapter(it.hourly)
+                binding.predictHourlyRecycler.adapter = adapter
+            }
+
+            viewModel.status.observe(viewLifecycleOwner) {
+                binding.weatherStatus1.text = it
+            }
         }
     }
 
